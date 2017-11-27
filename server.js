b544389dffe0b86a8dcb97e25ac179e818ad676c
xariniov9 <himanshutiw2794@gmail.com>
2017-11-27 22:19:59 +0530

    cors handled

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
+var cors = require('cors')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
